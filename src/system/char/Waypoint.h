2aac59e3ff0488a1b9a491229f1d0e4b7ef116d2
rjkiv <76180273+rjkiv@users.noreply.github.com>
2025-09-18 11:44:11 -0700

    some more char classes

--- src/system/char/Waypoint.h
+++ src/system/char/Waypoint.h
@@ -1,5 +1,8 @@
 #pragma once
+#include "math/Vec.h"
+#include "obj/Object.h"
 #include "rndobj/Trans.h"
+#include "utl/MemMgr.h"
 
 /** "A waypoint for character movement. Characters walk to
  *  these, start themselves out from these, etc." */
@@ -15,6 +18,41 @@ public:
     virtual void Load(BinStream &);
     virtual void Highlight();
 
+    OBJ_MEM_OVERLOAD(0x14)
+    NEW_OBJ(Waypoint)
+
+    static void Init();
+    static void Terminate();
+    static Waypoint *FindNearest(const Vector3 &, int);
+
+private:
+    static std::list<Waypoint *> *sWaypoints;
+    static int sSearchID;
+
+    static Waypoint *Find(int);
+    static DataNode OnWaypointFind(DataArray *);
+    static DataNode OnWaypointNearest(DataArray *);
+    static DataNode OnWaypointLast(DataArray *);
+
 protected:
     Waypoint();
+
+    /** "Flags for this waypoint, should be a bitfield per app" */
+    int mFlags; // 0xc0
+    /** "Radius within we can stop from a walk, or be tethered to" */
+    float mRadius; // 0xc4
+    /** "If positive, makes this shape a box with radius the x axis X half width,
+        and y_radius the Y axis half width" */
+    float mYRadius; // 0xc8
+    /** "Angular slop in degrees away from y axis" */
+    float mAngRadius; // 0xcc
+    int unkd0; // 0xd0
+    /** "degrees beyond ang radius you can never rotate past, if >= 0".
+        Ranges from -1 to 360. */
+    float mStrictAngDelta; // 0xd4
+    /** "how much beyond radius you will never leave,
+        it will forcibly pull you back, ignored if <= 0" */
+    float mStrictRadiusDelta; // 0xd8
+    /** "Waypoints we can walk to" */
+    ObjPtrVec<Waypoint> mConnections; // 0xdc
 };
