309dbdccf87da93f0c0e3db74779ef18b06b2856
rjkiv <76180273+rjkiv@users.noreply.github.com>
2025-10-22 16:06:07 -0700

    rndline

--- src/system/rndobj/Line.cpp
+++ src/system/rndobj/Line.cpp
@@ -1,17 +1,275 @@
 #include "rndobj/Line.h"
 #include "obj/Object.h"
 #include "math/Rot.h"
+#include "rndobj/Cam.h"
+#include "rndobj/Draw.h"
+#include "rndobj/Mat.h"
+#include "rndobj/Trans.h"
+#include "utl/BinStream.h"
 
-BEGIN_CUSTOM_PROPSYNC(RndLine::Point)
-    SYNC_PROP(point, o.point)
-    SYNC_PROP(color, o.color)
-    SYNC_PROP(alpha, o.color.alpha)
-END_CUSTOM_PROPSYNC
+RndLine *gLine;
+
+#pragma region Hmx::Object
 
 RndLine::RndLine()
     : mWidth(1), mHasCaps(true), mLinePairs(false), mFoldAngle(PI / 2), mMat(this),
-      unk134(true) {
+      mLineUpdate(true) {
     mMesh = Hmx::Object::New<RndMesh>();
+    mMesh->SetMutable(0x1F);
     mMesh->SetTransParent(this, false);
     UpdateInternal();
+}
+
+BEGIN_HANDLERS(RndLine)
+    HANDLE_EXPR(num_points, NumPoints())
+    HANDLE_ACTION(
+        set_point_pos,
+        SetPointPos(_msg->Int(2), Vector3(_msg->Float(3), _msg->Float(4), _msg->Float(5)))
+    )
+    HANDLE_EXPR(point_color, mPoints[_msg->Int(2)].color.Pack())
+    HANDLE_ACTION(
+        set_point_color,
+        SetPointColor(
+            _msg->Int(2),
+            Hmx::Color(_msg->Float(3), _msg->Float(4), _msg->Float(5), _msg->Float(6)),
+            true
+        )
+    )
+    HANDLE_ACTION(
+        set_points_color,
+        SetPointsColor(
+            _msg->Int(2),
+            _msg->Int(3),
+            Hmx::Color(_msg->Float(4), _msg->Float(5), _msg->Float(6), _msg->Float(7))
+        )
+    )
+    HANDLE_ACTION(set_update, SetUpdate(_msg->Int(2)))
+    HANDLE(set_mat, OnSetMat)
+    HANDLE_SUPERCLASS(RndDrawable)
+    HANDLE_SUPERCLASS(RndTransformable)
+    HANDLE_SUPERCLASS(Hmx::Object)
+END_HANDLERS
+
+BEGIN_CUSTOM_PROPSYNC(RndLine::Point)
+    SYNC_PROP(point, o.point)
+    SYNC_PROP_MODIFY(color, o.color, gLine->UpdatePointColor(_prop->Int(_i - 1), true))
+    SYNC_PROP_MODIFY(
+        alpha, o.color.alpha, gLine->UpdatePointColor(_prop->Int(_i - 1), true)
+    )
+END_CUSTOM_PROPSYNC
+
+BEGIN_PROPSYNCS(RndLine)
+    gLine = this;
+    SYNC_PROP_SET(mat, mMat.Ptr(), SetMat(_val.Obj<RndMat>()))
+    SYNC_PROP(width, mWidth)
+    SYNC_PROP_SET(fold_angle, mFoldAngle * RAD2DEG, mFoldAngle = _val.Float() * DEG2RAD;
+                  mFoldCos = cos(mFoldAngle))
+    SYNC_PROP_MODIFY(has_caps, mHasCaps, SetNumPoints(NumPoints()))
+    SYNC_PROP_MODIFY(line_pairs, mLinePairs, SetNumPoints(NumPoints()))
+    SYNC_PROP_SET(num_points, NumPoints(), SetNumPoints(_val.Int()))
+    SYNC_PROP_MODIFY(points, mPoints, )
+    SYNC_SUPERCLASS(RndDrawable)
+    SYNC_SUPERCLASS(RndTransformable)
+    SYNC_SUPERCLASS(Hmx::Object)
+END_PROPSYNCS
+
+BinStream &operator<<(BinStream &bs, const RndLine::Point &pt) {
+    bs << pt.point << pt.color;
+    return bs;
+}
+
+BEGIN_SAVES(RndLine)
+    SAVE_REVS(4, 0)
+    SAVE_SUPERCLASS(Hmx::Object)
+    SAVE_SUPERCLASS(RndDrawable)
+    SAVE_SUPERCLASS(RndTransformable)
+    bs << mMat << mPoints << mWidth << mFoldAngle << mHasCaps;
+    bs << mLinePairs;
+END_SAVES
+
+BEGIN_COPYS(RndLine)
+    CREATE_COPY_AS(RndLine, d);
+    MILO_ASSERT(d, 0x2D2);
+    COPY_SUPERCLASS(Hmx::Object)
+    COPY_SUPERCLASS(RndDrawable)
+    COPY_SUPERCLASS(RndTransformable)
+    COPY_MEMBER_FROM(d, mMat)
+    COPY_MEMBER_FROM(d, mPoints)
+    COPY_MEMBER_FROM(d, mWidth)
+    COPY_MEMBER_FROM(d, mFoldAngle)
+    COPY_MEMBER_FROM(d, mHasCaps)
+    COPY_MEMBER_FROM(d, mLinePairs)
+    UpdateInternal();
+END_COPYS
+
+BinStreamRev &operator>>(BinStreamRev &d, RndLine::Point &pt) {
+    d >> pt.point >> pt.color;
+    return d;
+}
+
+BEGIN_LOADS(RndLine)
+    LOAD_REVS(bs)
+    ASSERT_REVS(4, 0)
+    if (d.rev > 3) {
+        Hmx::Object::Load(bs);
+    }
+    RndDrawable::Load(bs);
+    if (d.rev < 3) {
+        ObjPtrList<Hmx::Object> objList(this);
+        int x;
+        bs >> x >> objList;
+    }
+    RndTransformable::Load(bs);
+    bs >> mMat;
+    d >> mPoints;
+    bs >> mWidth;
+    if (d.rev > 0) {
+        bs >> mFoldAngle;
+        d >> mHasCaps;
+    }
+    if (d.rev > 1) {
+        d >> mLinePairs;
+    }
+    UpdateInternal();
+END_LOADS
+
+inline TextStream &operator<<(TextStream &ts, const RndLine::Point &pt) {
+    ts << "\n\tv:" << pt.point << "\n\tc:" << pt.color;
+    return ts;
+}
+
+void RndLine::Print() {
+    TheDebug << "   points: " << mPoints << "\n";
+    TheDebug << "   width: " << mWidth << "\n";
+    TheDebug << "   foldAngle: " << mFoldAngle << "\n";
+    TheDebug << "   hasCaps: " << mHasCaps << "\n";
+    TheDebug << "   linePairs:" << mLinePairs << "\n";
+}
+
+#pragma endregion
+#pragma region RndDrawable
+
+void RndLine::UpdateSphere() {
+    Sphere s;
+    MakeWorldSphere(s, true);
+    Transform xfm;
+    FastInvert(WorldXfm(), xfm);
+    Multiply(s, xfm, s);
+    SetSphere(s);
+}
+
+float RndLine::GetDistanceToPlane(const Plane &p, Vector3 &v3) {
+    if (mPoints.empty())
+        return 0;
+    else {
+        WorldXfm();
+        float ret = 0;
+        bool first = true;
+        FOREACH (it, mPoints) {
+            float dot = p.Dot(it->point);
+            if (first || std::fabs(dot) < std::fabs(ret)) {
+                ret = dot;
+                first = false;
+                v3 = it->point;
+            }
+        }
+        return ret;
+    }
+}
+
+bool RndLine::MakeWorldSphere(Sphere &s, bool b2) {
+    if (b2) {
+        s.Zero();
+        FOREACH (it, mPoints) {
+            s.GrowToContain(Sphere(it->point, mWidth));
+        }
+        return true;
+    } else {
+        if (mSphere.GetRadius()) {
+            Multiply(mSphere, WorldXfm(), s);
+            return true;
+        } else
+            return false;
+    }
+}
+
+void RndLine::Mats(std::list<class RndMat *> &mats, bool) {
+    if (mMat) {
+        mats.push_back(mMat);
+    }
+}
+
+void RndLine::DrawShowing() {
+    if (mPoints.size() >= 2) {
+        if (mLineUpdate) {
+            RndCam *cam = RndCam::Current();
+            UpdateLine(cam->WorldXfm(), cam->NearPlane());
+            mMesh->SetWorldXfm(cam->WorldXfm());
+        }
+        mMesh->DrawShowing();
+    }
+}
+
+RndDrawable *RndLine::CollideShowing(const Segment &s, float &f, Plane &p) {
+    RndDrawable *d = mMesh->Collide(s, f, p);
+    return d ? this : d;
+}
+
+int RndLine::CollidePlane(const Plane &p) { return mMesh->CollidePlane(p); }
+
+#pragma endregion
+#pragma region RndLine
+
+void RndLine::SetMat(RndMat *mat) {
+    mMat = mat;
+    mMesh->SetMat(mat);
+}
+
+void RndLine::SetUpdate(bool b1) {
+    mLineUpdate = b1;
+    if (!mLineUpdate) {
+        Transform xfm(WorldXfm());
+        static Vector3 offset(0, -1, 0);
+        Multiply(offset, xfm, xfm.v);
+        UpdateLine(xfm, 0);
+        mMesh->SetLocalPos(offset);
+    }
+}
+
+void RndLine::SetPointPos(int i, const Vector3 &pos) {
+    MILO_ASSERT((i >= 0) && (i < mPoints.size()), 0x1CE);
+    mPoints[i].point = pos;
+}
+
+void RndLine::SetPointColor(int i, const Hmx::Color &color, bool sync) {
+    MILO_ASSERT((i >= 0) && (i < mPoints.size()), 0x1D5);
+    mPoints[i].color = color;
+    UpdatePointColor(i, sync);
+}
+
+void RndLine::UpdatePointColor(int i, bool sync) {
+    Point &pt = mPoints[i];
+    VertsMap vmap;
+    MapVerts(i, vmap);
+    vmap.v++->color = pt.color;
+    vmap.v++->color = pt.color;
+    if (vmap.t != 0) {
+        vmap.v++->color = pt.color;
+        vmap.v++->color = pt.color;
+    }
+    if (sync)
+        mMesh->Sync(0x1F);
+}
+
+void RndLine::UpdateInternal() {
+    mFoldCos = cos(mFoldAngle);
+    mMesh->SetMat(mMat);
+    SetNumPoints(mPoints.size());
+}
+
+DataNode RndLine::OnSetMat(const DataArray *array) {
+    RndMat *mat = array->Obj<RndMat>(2);
+    SetMat(mat);
+    SetShowing(mat);
+    return 0;
 }
--- src/system/rndobj/Line.h
+++ src/system/rndobj/Line.h
@@ -1,5 +1,8 @@
 #pragma once
 #include "math/Color.h"
+#include "math/Mtx.h"
+#include "math/Vec.h"
+#include "obj/Data.h"
 #include "obj/Object.h"
 #include "rndobj/Draw.h"
 #include "rndobj/Mat.h"
@@ -13,12 +16,20 @@ class RndLine : public RndDrawable, public RndTransformable {
 public:
     class Point {
     public:
+        Point() : point(0, 0, 0), color(1, 1, 1) {}
         /** "Position of point" */
         Vector3 point; // 0x0
         /** "Color of point" */
         Hmx::Color color; // 0x10
         int unk[10];
     };
+
+    class VertsMap {
+    public:
+        int t; // Type
+        RndMesh::Vert *v;
+    };
+
     // Hmx::Object
     virtual ~RndLine() { RELEASE(mMesh); }
     OBJ_CLASSNAME(Line);
@@ -37,17 +48,36 @@ public:
     virtual void DrawShowing();
     virtual RndDrawable *CollideShowing(const Segment &, float &, Plane &);
     virtual int CollidePlane(const Plane &);
-    virtual RndMesh *Mesh() const { return mMesh; }
+    // RndHighlightable
     virtual void Highlight() { RndDrawable::Highlight(); }
+    // RndLine
+    virtual RndMesh *Mesh() const { return mMesh; }
 
     OBJ_MEM_OVERLOAD(0x1A);
     NEW_OBJ(RndLine)
     static void Init() { REGISTER_OBJ_FACTORY(RndLine) }
 
+    int NumPoints() const { return mPoints.size(); }
+    Point &PointAt(int idx) { return mPoints[idx]; }
+    float GetWidth() const { return mWidth; }
+    void SetWidth(float w) { mWidth = w; }
+
+    void SetPointPos(int, const Vector3 &);
+    void SetPointColor(int, const Hmx::Color &, bool);
+    void SetPointsColor(int, int, const Hmx::Color &);
+    void SetUpdate(bool);
+    void UpdatePointColor(int, bool);
+    void SetMat(RndMat *);
+    void SetNumPoints(int);
+
 protected:
     RndLine();
 
     void UpdateInternal();
+    void UpdateLine(const Transform &, float);
+    void MapVerts(int, VertsMap &);
+
+    DataNode OnSetMat(const DataArray *);
 
     /** "Width of the line". Ranges from 0 to 1000. */
     float mWidth; // 0x100
@@ -62,5 +92,5 @@ protected:
     float mFoldCos; // 0x11c
     /** "Material the line uses." */
     ObjPtr<RndMat> mMat; // 0x120
-    bool unk134;
+    bool mLineUpdate; // 0x134
 };
