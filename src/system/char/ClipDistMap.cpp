e904cb5c09457315426242aa9d110d71ccd7f6f3
jsenior10 <jacobsenior4@gmail.com>
2025-11-26 21:26:32 +0000

    AAAHHHHHHHHH I HATE IT

--- src/system/char/ClipDistMap.cpp
+++ src/system/char/ClipDistMap.cpp
@@ -1,4 +1,8 @@
 #include "char/ClipDistMap.h"
+#include "char/CharClip.h"
+#include "char/CharUtl.h"
+#include "macros.h"
+#include "math/Utl.h"
 
 void FindWeights(
     std::vector<RndTransformable *> &transes,
@@ -25,6 +29,55 @@ void FindWeights(
 ClipDistMap::ClipDistMap(
     CharClip *clip1, CharClip *clip2, float f1, float f2, int i, const DataArray *a
 )
-    : mClipA(clip1), mClipB(clip2), mWeightData(a), mSamplesPerBeat(0),
+    : mClipA(clip1), mClipB(clip2), mWeightData(a), mSamplesPerBeat(8),
       mLastMinErr(kHugeFloat), mBeatAlign(f1), mBeatAlignOffset(0), mBlendWidth(f2),
-      mNumSamples(i) {}
+      mNumSamples(i) {
+    int h = CalcHeight();
+    int w = CalcWidth();
+    mDists.Resize(w, h);
+
+    mBeatAlignPeriod = mBeatAlign * mSamplesPerBeat + 0.5;
+
+    int temp;
+    if (mBeatAlignPeriod != 0) {
+        temp = mAStart * mSamplesPerBeat - mBStart * mSamplesPerBeat;
+        mBeatAlignOffset = temp - (temp / mBeatAlignPeriod) * mBeatAlignPeriod;
+
+        if (mBeatAlignOffset < 0) {
+            mBeatAlignOffset += mBeatAlignPeriod;
+        }
+    }
+}
+
+bool ClipDistMap::BeatAligned(int i1, int i2) {
+    int l1;
+    int l2;
+
+    if (mBeatAlignPeriod == 0) {
+        l1 = 0;
+    } else {
+        l2 = i1 - i2;
+        l1 = l2 - (l2 / mBeatAlignPeriod) * mBeatAlignPeriod;
+        if (l1 < 0) {
+            mBeatAlignPeriod += l1;
+        }
+    }
+
+    return mBeatAlignOffset - l1 == 0;
+}
+
+void ClipDistMap::FindNodes(float f1, float f2, float f3) {
+    std::vector<Node> nodes = mNodes;
+    mLastMinErr = f1;
+    float f5 = f2;
+    float f6;
+    float f4 = f2 * 0.44999998807907104;
+    if (f5 == 0.0) {
+        f4 = kHugeFloat;
+        f6 = f4;
+    } else if (f6 == 0.0) {
+        f6 = f4;
+    }
+
+    FindBestNodeRecurse(f1, f4, (f4 * 2.0 - f5), mAStart, mAEnd);
+}
